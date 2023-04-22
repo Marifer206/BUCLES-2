8cde3b929ec65707c685481757d5c7c2e49a72bc
Maria Fernanda Leon Montoya <124616296+Marifer206@users.noreply.github.com>
2023-04-22 16:55:46 -0500

    Update PUNTO #5 RETO 8.py

--- PUNTO #5 RETO 8.py
+++ PUNTO #5 RETO 8.py
@@ -1,7 +1,7 @@
-n = int(input("Introduce un número entero al que desea elevar 2: ")) # pedimos al usuario que introduzca el valor de n
+n = int(input("Introduce un número entero al que desea elevar n: ")) # solicitar al usuario que introduzca el valor de n
 resultado = 1 # valor inicial del resultado
 
-for i in range(n): # Iniciamos un ciclo for que se ejecuta n veces
-    resultado *= 2 # En cada iteración, multiplicamos resultado por 2 y lo asignamos a resultado
+for i in range(n): # Iniciar un ciclo for que se ejecuta n veces
+    resultado *= 2 # En cada iteración, multiplica el resultado por 2 y lo asigna a resultado
 
-print("El resultado de 2 elevado a la " + str(n)+ " es " + str(resultado)) # resultado final: 2 elevado a la potencia n
+print("El resultado de 2 elevado a la " + str(n)+ " es " + str(resultado)) # imprime el resultado final de 2 elevado a n 
